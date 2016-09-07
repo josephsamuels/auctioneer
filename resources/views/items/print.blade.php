5092891f420462840dd9bdfc3753e03a35a24906
josephsamuels <joseph.w.samuels@gmail.com>
2016-09-07 19:03:46 -0400

    master - Updates to ticket layout.

--- resources/views/items/print.blade.php
+++ resources/views/items/print.blade.php
@@ -3,15 +3,25 @@
 @section('content')
     <div ng-controller="ItemsPrintController as vm" ng-cloak>
         <div class="col-xs-6" ng-repeat="item in vm.items | orderBy:'id'">
-            <div class="card"  style="height: 230px; overflow: hidden; page-break-inside:avoid; page-break-after:auto;">
+            <div class="card" style="height: 230px; overflow: hidden; page-break-inside:avoid; page-break-after:auto;">
                 <div class="card-block">
                     <div style="float: right" class="form-inline">
-                        <strong>Lot #:</strong>
-                        <input class="form-control form-control-sm" type="text" style="width: 80px;" />
+                        <div class="form-group">
+                            <strong>Lot #:</strong>
+                            <input class="form-control form-control-sm" type="text" style="width: 80px;"/>
+                        </div>
                     </div>
                     <p class="card-text"><strong>Item:</strong> [{[ item.short_description ]}]</p>
-                    <p class="card-text"><strong>Source:</strong> [{[ item.source ]}]</p>
-                    <p class="card-text"><strong>Description:</strong> [{[ item.full_description ]}]</p>
+                    <p class="card-text" style="font-size: 0.8rem;"><strong>Source:</strong> [{[ item.source ]}]</p>
+                    <p class="card-text" style="font-size: 0.8rem;"><strong>Description:</strong>
+                        [{[ item.full_description ]}]
+                    </p>
+                    <div class="form-inline" style="position: absolute; bottom: 20px">
+                        <strong>Winner:</strong>
+                        <input class="form-control form-control-sm" type="text" style="width: 220px;"/>
+                        <strong>Bid:</strong>
+                        <input class="form-control form-control-sm" type="text" style="width: 100px;"/>
+                    </div>
                 </div>
             </div>
         </div>
